20e809110f3ab17d674ce3c0d43769e553fa2204
Rickard Armiento <rickard-gpg@armiento.net>
2025-05-03 10:46:52 +0200

    Improve udev rules for hot-plugging video devices

--- dependencies/installers/Ubuntu-24.04/install.sh
+++ dependencies/installers/Ubuntu-24.04/install.sh
@@ -2,14 +2,11 @@
 
 sudo apt-get install -y crun podman podman-compose curl wmctrl xdotool python3-tk python3-gi python3-gst-1.0 gstreamer1.0-pipewire gir1.2-appindicator3-0.1 yq mesa-utils
 
-# Create a udev rule to place camera devices inside /dev/cameras instead to allow easy hot-pluggable sharing of all camera video devices
-# You can skip this if you do not care about being able to hot-plug video devices inside running containers
+# Create a udev rule to duplicate the video device nodes inside /dev/video to allow hot-plugging of cameras into containers
+# If you skip this, everything will work except you cannot hot-plug video devices to already running software inside containers
 sudo tee /etc/udev/rules.d/99-cdc.rules <<EOF
-ACTION=="add", SUBSYSTEM=="cpu", RUN+="/bin/mkdir -p -m 0755 /dev/video"
-ACTION=="add", SUBSYSTEM=="video4linux", RUN+="/bin/mkdir -p -m 0755 /dev/video"
-KERNEL=="video*", SUBSYSTEM=="video4linux", ACTION=="add", RUN+="/bin/mv /dev/%k /dev/video/%k"
-KERNEL=="video*", SUBSYSTEM=="video4linux", ACTION=="add", RUN+="/bin/ln -s /dev/video/%k /dev/%k"
-KERNEL=="video*", SUBSYSTEM=="video4linux", ACTION=="remove", RUN+="/bin/rm -f /dev/video/%k /dev/%k"
+KERNEL=="video*", SUBSYSTEM=="video4linux", ACTION=="add", RUN+="/bin/bash -c '/bin/mkdir -p -m 0755 /dev/video; MAJOR=\$\$(stat -c %%t /dev/%k | xargs printf \"%%d\"); MINOR=\$\$(stat -c %%T /dev/%k | xargs printf \"%%d\"); mknod /dev/video/%k c \$\$MAJOR \$\$MINOR; chown root:video /dev/video/%k; chmod 0660 /dev/video/%k'"
+KERNEL=="video*", SUBSYSTEM=="video4linux", ACTION=="remove", RUN+="/bin/rm -f /dev/video/%k"
 EOF
 sudo udevadm control --reload-rules && sudo udevadm trigger
 
