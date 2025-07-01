22ef98aaf2c1cf6902dfab54d687d2fd49e6d84b
Rickard Armiento <rickard-gpg@armiento.net>
2025-07-01 21:02:10 +0200

    Update to new image build system; and cdc-entrypoint

--- images/image-u24/build.sh
+++ images/image-u24/build.sh
@@ -9,8 +9,6 @@ if [ -z "$USER" -o -z "$UID" -o -z "$LANG" ]; then
     exit 1
 fi
 
-BASE="ubuntu:24.04"
-
 # The sed 's/.utf8/.UTF-8/' fixes incorrectly specified locales from pre-GNOME 3.18 I think.
 LOCALES="$(locale -a | grep -v "POSIX" | sed 's/.utf8/.UTF-8/' | tr '\n' ' ')"
 
@@ -20,33 +18,89 @@ COMMANDS_FILES=""
 PRECOMMANDS_FILES=""
 
 CDC_CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/cdc"
+mkdir -p "$CDC_CONFIG_DIR/image-u24/build/files" "$CDC_CONFIG_DIR/image-u24/build/tools"
 mkdir -p "$CDC_CONFIG_DIR/image-u24/installed"
-ln -sf "$SCRIPTPATH/opt" "$CDC_CONFIG_DIR/image-u24/opt"
+mkdir -p "$CDC_CONFIG_DIR/image-u24/requested"
+
+(
+    cd "$SCRIPTPATH"/requested-default
+    for FILE in *; do
+        if [ ! -e  "$CDC_CONFIG_DIR/image-u24/requested/${FILE}.disabled" ]; then
+            cp "$FILE" "$CDC_CONFIG_DIR/image-u24/requested/."
+        fi
+    done
+)
+
+if [ ! -e  "$CDC_CONFIG_DIR/image-u24/requested/50-default-commands-time.disabled" ]; then
+  LOCALTIME=$(readlink /etc/localtime)
+  if [ -n "$LOCALTIME" ]; then
+      TZ_LOCATION="${LOCALTIME##*/}"
+      TZ_AREA="${LOCALTIME%/*}"
+      TZ_AREA="${TZ_AREA##*/}"
+      echo "RUN ln -sf \"/usr/share/zoneinfo/${TZ_AREA}/${TZ_LOCATION}\" /etc/localtime" > "$CDC_CONFIG_DIR/image-u24/requested/50-default-commands-time"
+  else
+      cp /etc/localtime files/localtime
+      echo "RUN rm -f /etc/localtime" > "$CDC_CONFIG_DIR/image-u24/requested/50-default-commands-time"
+      echo "COPY files/localtime /etc/localtime" >> "$CDC_CONFIG_DIR/image-u24/requested/50-default-commands-time"
+  fi
+fi
+
+if [ ! -e  "$CDC_CONFIG_DIR/image-u24/requested/51-default-commands-locale.disabled" ]; then
+  cat >> "$CDC_CONFIG_DIR/image-u24/requested/51-default-commands-locale" <<EOF
+COPY ./tools/en_SE.locale /tmp/en_SE.locale
+RUN test ! -e /usr/share/i18n/locales/en_SE && cp /tmp/en_SE.locale /usr/share/i18n/locales/en_SE && localedef -i en_SE -f UTF-8 en_SE.UTF-8 && echo "# en_SE.UTF-8 UTF-8" >> "/etc/locale.gen" && echo "en_SE.UTF-8 UTF-8" >> "/usr/share/i18n/SUPPORTED"
+RUN locale-gen ${LOCALES} && update-locale "LANG=$LANG"
+ENV LANG $LANG
+RUN groupadd -r -g 5000 build && useradd -m -u 5000 -g 5000 -c "Build user" "build"
+EOF
+fi
+
+FULLNAME="$(getent passwd rar | awk -F':' '{print $5}')"
+
+if [ ! -e  "$CDC_CONFIG_DIR/image-u24/requested/52-default-commands-user.disabled" ]; then
+  # We need to handle the user part differently depnding on if it overlaps the default 1000 user or not.
+  if [ "$UID" == "1000" ]; then
+
+    cat >> "$CDC_CONFIG_DIR/image-u24/requested/52-default-commands-user" <<EOF
+RUN usermod -l "$USER" ubuntu && groupmod -n "$USER" ubuntu && usermod -d "/home/$USER" -m "$USER" && usermod -c "$FULLNAME" "$USER" && mkdir /tmp/$USER && chown "$USER:$USER" "/tmp/$USER" && chmod 0700 "/tmp/$USER" && mkdir "/tmp/$USER/run" && chown "$USER:$USER" "/tmp/$USER/run" && chmod 0700 "/tmp/$USER/run"
+EOF
+
+  else
+
+    cat >> "$CDC_CONFIG_DIR/image-u24/requested/52-default-commands-user" <<EOF
+RUN groupadd -r -g "$UID" "$USER" && useradd -m -u "$UID" -g "$UID" -c "$FULLNAME" "$USER" && mkdir /tmp/$USER && chown "$USER:$USER" "/tmp/$USER" && chmod 0700 "/tmp/$USER" && mkdir "/tmp/$USER/run" && chown "$USER:$USER" "/tmp/$USER/run" && chmod 0700 "/tmp/$USER/run"
+EOF
+
+  fi
+  cat >> "$CDC_CONFIG_DIR/image-u24/requested/52-default-commands-user" <<EOF
+RUN mkdir -p /run/user && ln -s "/tmp/$USER/run" "/run/user/${UID}"
+EOF
+fi
 
 PKGS=()
 PKGS_NODEPS=()
 COMMANDS=()
 PRECOMMANDS=()
 
-if [ -n "$(ls "$CDC_CONFIG_DIR/image-u24/installed")" ]; then
-    for APP_DIR in "$CDC_CONFIG_DIR"/image-u24/installed/*; do
-	echo "Checking image dependencies in $APP_DIR"
-	PKGS=( $(yq -r '."x-application".images.u24.pkgs[]? // empty' "$APP_DIR/compose.yaml") )
-	echo "Picked up pkgs:" "${PKGS[@]}"
-	PKGS_ONLY=( $(yq -r '."x-application".images.u24."pkgs-only[]?" // empty' "$APP_DIR/compose.yaml") )
-	echo "Picked up pkgs to be installed without recommendeds:" "${PKGS_ONLY[@]}"
-	PRECOMMANDS=( $(yq -r '."x-application".images.u24.precommands[]? // empty' "$APP_DIR/compose.yaml") )
-	echo "Picked up pre-commands:" "${PRECOMMANDS[@]}"
-	COMMANDS=( $(yq -r '."x-application".images.u24.commands[]? // empty' "$APP_DIR/compose.yaml") )
-	echo "Picked up commands:" "${COMMANDS[@]}"
-    done
-fi
+#if [ -n "$(ls "$CDC_CONFIG_DIR/image-u24/installed")" ]; then
+#    for APP_DIR in "$CDC_CONFIG_DIR"/image-u24/installed/*; do
+#	echo "Checking image dependencies in $APP_DIR"
+#	PKGS=( $(yq -r '."x-application".images.u24.pkgs[]? // empty' "$APP_DIR/compose.yaml") )
+#	echo "Picked up pkgs:" "${PKGS[@]}"
+#	PKGS_ONLY=( $(yq -r '."x-application".images.u24."pkgs-only[]?" // empty' "$APP_DIR/compose.yaml") )
+#	echo "Picked up pkgs to be installed without recommendeds:" "${PKGS_ONLY[@]}"
+#	PRECOMMANDS=( $(yq -r '."x-application".images.u24.precommands[]? // empty' "$APP_DIR/compose.yaml") )
+#	echo "Picked up pre-commands:" "${PRECOMMANDS[@]}"
+#	COMMANDS=( $(yq -r '."x-application".images.u24.commands[]? // empty' "$APP_DIR/compose.yaml") )
+#	echo "Picked up commands:" "${COMMANDS[@]}"
+#    done
+#fi
 
 IFS=$'\n'
-PKGS+=( $(ls image/*-pkgs* 2>/dev/null | grep -v "~" | sort -s -t- -k1,1n -k2 | xargs cat) )
-PKGS_ONLY+=( $(ls image/*-pkgs-only* 2>/dev/null | grep -v "~" | sort -s -t- -k1,1n -k2 | xargs cat) )
-COMMANDS+=( $(ls image/*-commands* 2>/dev/null | grep -v "~" | sort -s -t- -k1,1n -k2 | xargs cat) )
-PRECOMMANDS+=( $(ls image/*-precommands* 2>/dev/null | grep -v "~" | sort -s -t- -k1,1n -k2 | xargs cat) )
+PKGS+=( $(ls "$CDC_CONFIG_DIR"/image-u24/requested/*-pkgs* 2>/dev/null | grep -v "~" | sort -s -t- -k1,1n -k2 | xargs cat) )
+PKGS_ONLY+=( $(ls "$CDC_CONFIG_DIR"/image-u24/requested/*-pkgs-only* 2>/dev/null | grep -v "~" | sort -s -t- -k1,1n -k2 | xargs cat) )
+COMMANDS+=( $(ls "$CDC_CONFIG_DIR"/image-u24/requested/*-commands* 2>/dev/null | grep -v "~" | sort -s -t- -k1,1n -k2 | xargs cat) )
+PRECOMMANDS+=( $(ls "$CDC_CONFIG_DIR"/image-u24/requested/*-precommands* 2>/dev/null | grep -v "~" | sort -s -t- -k1,1n -k2 | xargs cat) )
 IFS=' '
 
 if [ -n "${PKGS}" ]; then
@@ -62,94 +116,22 @@ if [ -n "${PRECOMMANDS}" ]; then
     PRECOMMANDS_LINES="$(printf "%s\n" "${PRECOMMANDS[@]}")"
 fi
 
+## Collect necessary files and tools in the build context directory
 mkdir -p files
-cp /etc/timezone files/timezone
+cp /etc/timezone "$CDC_CONFIG_DIR/image-u24/build/files"
+cp -rp "$SCRIPTPATH"/tools/* "$CDC_CONFIG_DIR"/image-u24/build/tools/.
 
-cat > ./Containerfile <<EOF
-FROM $BASE
-ENV DEBIAN_FRONTEND=noninteractive
-COPY files/timezone /etc/timezone
-EOF
-
-cat >> ./Containerfile <<EOF
+## Create containerfile
+cat >> "$CDC_CONFIG_DIR/image-u24/build/Containerfile" <<EOF
 $PRECOMMANDS_LINES
-RUN dpkg --add-architecture i386 && apt-get update && apt-get -y dist-upgrade && apt-get install -y --reinstall ca-certificates locales
 $PKGS_ONLY_LINES
 $PKGS_LINES
-RUN apt-get clean autoclean -y && apt-get autoremove -y && rm -rf /var/tmp/* && rm -rf /tmp/*
 $COMMANDS_LINES
 EOF
 
-# Test for NVIDIA GPU
-if command -v nvidia-smi 2>/dev/null; then
-    cat >> ./Containerfile <<EOF
-EOF
-fi
-
-# Test for intel GPU
-if [ "1" == "0" ]; then
-    cat >> ./Containerfile <<EOF
-RUN mkdir -p /tmp/gpu && \
- cd /tmp/gpu && \
- wget https://github.com/oneapi-src/level-zero/releases/download/v1.21.9/level-zero_1.21.9+u24.04_amd64.deb && \
- wget https://github.com/intel/intel-graphics-compiler/releases/download/v2.8.3/intel-igc-core-2_2.8.3+18762_amd64.deb && \
- wget https://github.com/intel/intel-graphics-compiler/releases/download/v2.8.3/intel-igc-opencl-2_2.8.3+18762_amd64.deb && \
- wget https://github.com/intel/compute-runtime/releases/download/25.09.32961.7/intel-level-zero-gpu_1.6.32961.7_amd64.deb && \
- wget https://github.com/intel/compute-runtime/releases/download/25.09.32961.7/intel-opencl-icd_25.09.32961.7_amd64.deb && \
- wget https://github.com/intel/compute-runtime/releases/download/25.09.32961.7/libigdgmm12_22.6.0_amd64.deb && \
- dpkg -i *.deb && \
- rm *.deb
-EOF
-fi
-
-# Test for AMD GPU
-if [ "1" == "0" ]; then
-    cat >> ./Containerfile <<EOF
-EOF
-fi
-
-LOCALTIME=$(readlink /etc/localtime)
-if [ -n "$LOCALTIME" ]; then
-    TZ_LOCATION="${LOCALTIME##*/}"
-    TZ_AREA="${LOCALTIME%/*}"
-    TZ_AREA="${TZ_AREA##*/}"
-    echo "RUN ln -sf \"/usr/share/zoneinfo/${TZ_AREA}/${TZ_LOCATION}\" /etc/localtime" >> ./Containerfile
-else
-    cp /etc/localtime files/localtime
-    echo "RUN rm -f /etc/localtime" >> ./Containerfile
-    echo "COPY files/localtime /etc/localtime" >> ./Containerfile
-fi
-
-cat >> ./Containerfile <<EOF
-COPY ./tools/en_SE.locale /tmp/en_SE.locale
-RUN test ! -e /usr/share/i18n/locales/en_SE && cp /tmp/en_SE.locale /usr/share/i18n/locales/en_SE && localedef -i en_SE -f UTF-8 en_SE.UTF-8 && echo "# en_SE.UTF-8 UTF-8" >> "/etc/locale.gen" && echo "en_SE.UTF-8 UTF-8" >> "/usr/share/i18n/SUPPORTED"
-RUN locale-gen ${LOCALES} && update-locale "LANG=$LANG"
-ENV LANG $LANG
-RUN groupadd -r -g 5000 build && useradd -m -u 5000 -g 5000 -c "Build user" "build"
-EOF
-
-FULLNAME="$(getent passwd rar | awk -F':' '{print $5}')"
-
-# We need to handle the user part differently depnding on if it overlaps the default 1000 user or not.
-if [ "$UID" == "1000" ]; then
-
-    cat >> Containerfile <<EOF
-RUN usermod -l "$USER" ubuntu && groupmod -n "$USER" ubuntu && usermod -d "/home/$USER" -m "$USER" && usermod -c "$FULLNAME" "$USER" && mkdir /tmp/$USER && chown "$USER:$USER" "/tmp/$USER" && chmod 0700 "/tmp/$USER" && mkdir "/tmp/$USER/run" && chown "$USER:$USER" "/tmp/$USER/run" && chmod 0700 "/tmp/$USER/run"
-EOF
-
-else
-
-    cat >> Containerfile <<EOF
-RUN groupadd -r -g "$UID" "$USER" && useradd -m -u "$UID" -g "$UID" -c "$FULLNAME" "$USER" && mkdir /tmp/$USER && chown "$USER:$USER" "/tmp/$USER" && chmod 0700 "/tmp/$USER" && mkdir "/tmp/$USER/run" && chown "$USER:$USER" "/tmp/$USER/run" && chmod 0700 "/tmp/$USER/run"
-EOF
-
-fi
-
-cat >> Containerfile <<EOF
-RUN mkdir -p /run/user && ln -s "/tmp/$USER/run" "/run/user/${UID}"
-EOF
-
+cd "$CDC_CONFIG_DIR/image-u24/build/"
 podman build -t cdc-u24 --label=wrap .
 podman image prune -f --filter label=wrap
 
+# After a container has been created, it may take a significant time to instansitate it the first time
 podman run --rm -w "/home/$USER" --user="$USER" --shm-size=1G --cap-drop=ALL --read-only --read-only-tmpfs --userns=keep-id --name "cdc_test_u24" cdc-u24 echo "Container finished."
